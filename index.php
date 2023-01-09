1abe8fc2c03e0b9d20999df7d3b7364f0e388a9a
DiceglieA <angelodiceglie10@gmail.com>
2023-01-09 16:54:27 +0100

    last commit

--- index.php
+++ index.php
@@ -11,11 +11,18 @@ class Movie {
         $this->genre = $_genre;
         $this->date = $_date;
     }
+    public function set_date() {
+        if (is_numeric($this->date)) {
+            return "<h2>è un numero</h2>";
+        } else {
+            return "<h2>non è un numero</h2>";
+        };
+    }
 }
 ;
 $firstmovie = new Movie('In viaggio con pippo', 'Kevin Lima', 'animation', '1995');
 $secondmovie = new Movie('Le follie dell imperatore', 'Mark Dindal', 'animation', '2000');
 
 var_dump($firstmovie);
-var_dump($secondmovie)
+var_dump($secondmovie);
 ?>
