d61c8109e143f2babd872ff9f383783a2e79e20d
DiceglieA <angelodiceglie10@gmail.com>
2023-01-09 16:37:40 +0100

    stampati a schermo

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
-<?php
+<?
 class Movie {
     public $title;
     public $director;
     public $genre;
     public $date;
 
-    function __constructor($title, $director, $genre, $date) {
-        $this->title = $title;
-        $this->director = $director;
-        $this->genre = $genre;
-        $this->date = $date;
+    public function __construct($_title, $_director, $_genre, $_date) {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->genre = $_genre;
+        $this->date = $_date;
     }
-} 
+}
+;
+$firstmovie = new Movie('In viaggio con pippo', 'Kevin Lima', 'animation', '1995');
+$secondmovie = new Movie('Le follie dell imperatore', 'Mark Dindal', 'animation', '2000');
+
+var_dump($firstmovie);
+var_dump($secondmovie)
 ?>
